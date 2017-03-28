6dd8acc9b32c3d1b2b78bfb6322880e3122a96c1
kuntiki <luismartinez@Luiss-iMac.local>
2017-03-28 07:51:14 -0500

    upload bookings
    added the insert code to the upload page

--- imports/ui/components/uploads.js
+++ imports/ui/components/uploads.js
@@ -1,11 +1,16 @@
 import { Template } from 'meteor/templating';
-
 import Papa from 'papaparse';
+
+import { Bookings } from '../../api/bookings/bookings.js';
 
 import './uploads.html';
 
 Template.uploads.onCreated( function uploadsOnCreated() {
 	Template.instance().uploading = new ReactiveVar( false );
+
+	this.autorun(() => {
+		this.subscribe('Bookings');
+	});
 });
 
 Template.uploads.helpers({
@@ -23,6 +28,36 @@ Template.uploads.events({
 			header: true,
 			complete(results, file) {
 				console.log(results);
+				// insert data
+				for ( let i = 0; i < results.data.length; i++ ) {
+					let item   = results.data[ i ],
+					exists = Bookings.findOne( { code: item.Propuesta } );
+
+					if ( !exists ) {
+						var newBooking = {
+							code: item.Propuesta,
+							title: item.Titulo,
+							value: +item["Valor Estimado"],
+							currency: item.Moneda,
+							brm: item["Promotor / BRM"],
+							statusDate: item["Fecha Estatus"],
+							hours: item["Esfuerzo (Horas)"],
+							labor: +item["Costo Nómina"],
+							practice: item["Práctica"],
+							expenses: +item["Valor Gastos"],
+						};
+						newBooking.customerCode = newBooking.code.substr(0, 4);
+						newBooking.bookingConsecutive = newBooking.code.substr(5, 3);
+						newBooking.bookedMonth = Number(newBooking.statusDate.substr(3, 2));
+						newBooking.bookedYear = Number(newBooking.statusDate.substr(6, 4));
+						newBooking.adjHours = newBooking.hours;
+						newBooking.adjBrm = newBooking.brm;
+
+						Bookings.insert( newBooking );
+					} else {
+						console.warn( 'Rejected. This item already exists.' );
+					}
+				}
 				// show form again
 				template.uploading.set(false);
 			},
